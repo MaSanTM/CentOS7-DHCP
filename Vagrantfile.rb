acf73592262b02211d27af98459970e3bc6a123d
Mateus Santos <mateus.h.santos@outlook.com.br>
2021-07-01 22:26:06 -0300

    Create Vagrantfile

---
+++ Vagrantfile.rb
@@ -0,0 +1,10 @@
+Vagrant.configure("2") do |config|
+    config.vm.box = "centos/7"
+      config.disksize.size = "50GB"
+      config.vm.network "private_network", type: "dhcp"
+      config.vm.provider "virtualbox" do |vb|
+        vb.name = "CentOS7"
+        vb.memory = 2048
+        vb.cpus = 2
+      end
+end
